9ca8a2ad4a03c59eb281dbfe57e58559f04892fd
tirth-2001 <tirthgpatel.27@gmail.com>
2022-06-19 14:59:48 +0530

    app content modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
-import React from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 function App() {
 	return (
 		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>This is a react app deployed on GCP</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Learn React
-				</a>
-			</header>
+			<p>Hi from Google Cloud</p>
 		</div>
 	)
 }
